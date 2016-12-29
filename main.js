b2b36c2f32e5bfd8968a87f9869a7d09ecf987bb
yovannanetwork <yovannanetwork@gmail.com>
2016-12-29 13:19:53 -0600

    enlace 2

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ var horas, pago;
 var horas = prompt("Hola, ingresa tus horas trabajadas");
 var pago = prompt("Ahora, ingresa cual es el pago por hora");
 
-alert("tu sueldo es" + (pago*horas));
+alert("tu sueldo es" +   (pago*horas));
